ce04a0410fc91c9551ba8e027159b3a358425a45
Jake <zhandos38@gmail.com>
2022-05-05 10:43:46 +0600

    fixes

--- backend/controllers/AnswerController.php
+++ backend/controllers/AnswerController.php
@@ -105,7 +105,7 @@ class AnswerController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         }
 
         return $this->render('update', [
