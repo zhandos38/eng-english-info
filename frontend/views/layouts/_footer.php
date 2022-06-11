f6c3aefe20c2f98189220007e1f05a0b1b051e8b
zhandos38 <zhandos38@gmail.com>
2022-06-11 14:34:47 +0600

    fixes

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -19,7 +19,7 @@
             <div class="col-8 col-md-3">
                 <ul>
                     <li><a href="#"><i class="fa fa-map-marker"></i>&nbsp;United States of Shymkent</a></li>
-                    <li><a href="#"><i class="fa fa-phone"></i>&nbsp;+7(701) 771-45-47</a></li>
+                    <li><a href="#"><i class="fa fa-phone"></i>&nbsp;+7(771)328-52-98</a></li>
                 </ul>
             </div>
             <div class="col-8 col-md-3">
@@ -29,6 +29,6 @@
                 </ul>
             </div>
         </div>
-        <div class="dev-info"><span>Автор: Болат Насима Нышанқызы. Группа: К111-17. Все права защищены - 2022.&nbsp;&nbsp;</span><span>With&nbsp;&nbsp;<i class="fa fa-heart"></i>&nbsp;</span><a href="http://itbgroup.kz">Itbgroup.kz</a></div>
+        <div class="dev-info"><span>Автор: Болат Насима Нышанқызы. Группа: К111-17. Все права защищены - 2022.&nbsp;&nbsp;</span><span>With&nbsp;&nbsp;<i class="fa fa-heart"></i>&nbsp;</span></div>
     </div>
 </footer>
